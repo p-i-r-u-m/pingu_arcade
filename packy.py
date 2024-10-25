d199a05ab25f26fd0c01619503ca881479288e44
p-i-r-u-m <igr.case@gmail.com>
2024-10-25 19:58:34 +0300

    Edjust packy to current needs

--- packy.py
+++ packy.py
@@ -3,31 +3,17 @@ from functions_packy.packy_functions import build_cmake, create_report, run_veri
 
 def main():
 
-    parser = argparse.ArgumentParser(description="Packy - your friendly helper in passing labs")
+    parser = argparse.ArgumentParser(description="Packy - your friendly helper in automated developing")
 
     # Adding command-line arguments
     # parser.add_argument('-h', '--help', action='store_true', help='Packy help info')
     parser.add_argument('-c', '--cmake', action='store_true', help='Build project with CMake')
-    parser.add_argument('-r', '--report', action='store_true', help='Creating report docx file')
-    parser.add_argument('-v', '--verify', action='store_true', help='Verify report data')
-    parser.add_argument('-e', '--email', action='store_true', help='Send email with the report')
 
     args = parser.parse_args()
 
     if args.cmake:
         print("Building project with CMake...")
         build_cmake()
-
-    if args.report:
-        print("Start creating report...\n")
-        create_report()
-
-    if args.verify:
-        run_verify()
-
-    if args.email:
-        print("Sending report...")
-        send_email()
         
 if __name__ == "__main__":
     main()
